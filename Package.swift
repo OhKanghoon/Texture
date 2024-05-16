6dc3041e9e79f34f52e407e4799cb0b88e32eac5
OhKanghoon <ggaa96@naver.com>
2024-05-17 00:39:06 +0900

    fix: add PINRemoteImage dependency

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 let package = Package(
   name: "Texture",
   platforms: [
-    .macOS(.v10_13),
-    .iOS(.v12),
-    .tvOS(.v12),
+    .iOS(.v14),
+    .macOS(.v11),
+    .tvOS(.v14),
   ],
   products: [
     .library(
@@ -15,11 +15,15 @@ let package = Package(
       targets: ["AsyncDisplayKit"]
     ),
   ],
-  dependencies: [],
+  dependencies: [
+    .package(url: "https://github.com/pinterest/PINRemoteImage.git", from: "3.0.0"),
+  ],
   targets: [
     .target(
       name: "AsyncDisplayKit",
-      dependencies: [],
+      dependencies: [
+        "PINRemoteImage",
+      ],
       path: "spm/Sources/AsyncDisplayKit",
       publicHeadersPath: "include",
       cSettings: [
@@ -28,13 +32,7 @@ let package = Package(
         .define("AS_USE_PHOTOS", to: "1"),
         .define("AS_USE_PHOTOS", to: "1"),
         .define("AS_USE_ASSETS_LIBRARY", to: "1"),
-      ],
-      cxxSettings: [
-        .define("AS_USE_VIDEO", to: "1"),
-        .define("AS_USE_MAPKIT", to: "1"),
-        .define("AS_USE_PHOTOS", to: "1"),
-        .define("AS_USE_PHOTOS", to: "1"),
-        .define("AS_USE_ASSETS_LIBRARY", to: "1"),
+        .define("AS_PIN_REMOTE_IMAGE", to: "1"),
       ]
     ),
   ],
--- Source/Details/ASPINRemoteImageDownloader.mm
+++ Source/Details/ASPINRemoteImageDownloader.mm
@@ -16,23 +16,34 @@
 #import "ASThread.h"
 #import "ASImageContainerProtocolCategories.h"
 
-#if __has_include (<PINRemoteImage/PINGIFAnimatedImage.h>)
+#if __has_include (<PINRemoteImage/PINGIFAnimatedImage.h>) || __has_include("PINGIFAnimatedImage.h")
 #define PIN_ANIMATED_AVAILABLE 1
+#if __has_include(<PINRemoteImage/PINGIFAnimatedImage.h>)
 #import <PINRemoteImage/PINCachedAnimatedImage.h>
 #import <PINRemoteImage/PINAlternateRepresentationProvider.h>
+#else
+#import "PINCachedAnimatedImage.h"
+#import "PINAlternateRepresentationProvider.h"
+#endif
 #else
 #define PIN_ANIMATED_AVAILABLE 0
 #endif
 
-#if __has_include(<webp/decode.h>)
+#if __has_include(<webp/decode.h>) || __has_include("webp/decode.h")
 #define PIN_WEBP_AVAILABLE  1
 #else
 #define PIN_WEBP_AVAILABLE  0
 #endif
 
+#if __has_include(<PINRemoteImage/PINRemoteImage.h>)
 #import <PINRemoteImage/PINRemoteImageManager.h>
 #import <PINRemoteImage/NSData+ImageDetectors.h>
 #import <PINRemoteImage/PINRemoteImageCaching.h>
+#else
+#import "PINRemoteImageManager.h"
+#import "NSData+ImageDetectors.h"
+#import "PINRemoteImageCaching.h"
+#endif
 
 static inline PINRemoteImageManagerPriority PINRemoteImageManagerPriorityWithASImageDownloaderPriority(ASImageDownloaderPriority priority) {
   switch (priority) {
